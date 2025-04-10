84d0f150a00763fcc7965f707a42a4acb08190a7
DMorris0045 <158842078+DMorris0045@users.noreply.github.com>
2025-04-10 14:42:24 -0500

    Delete project4_dsm0045_Morris directory

--- project4_dsm0045_Morris/Phase1/node.cpp
+++
@@ -1,8 +0,0 @@
-#include "node.h"
-
-node::node(int id) {
-    this->id = id;              // question number
-    this->next = nullptr;       // points to the next node in the list
-    this->pointVal = 0.0;       // defualt point sccore
-}
-
--- project4_dsm0045_Morris/Phase2/node.cpp
+++
@@ -1,9 +0,0 @@
-#include "node.h"
-
-//constructor for nodes
-// initializes a question with a given node id
-node::node(int id) {
-    this->id = id; //sets node id
-    this->next = nullptr; // initializes the next pointer to null
-    this->pointVal = 0.0; // default point value
-}
